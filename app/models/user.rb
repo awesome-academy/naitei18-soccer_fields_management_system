00c8457232fb7283d101244b840eff24e04c36d4
Ngoc Nguyen <nguyenthaingoc162002@gmail.com>
2023-07-20 16:04:10 +0700

    Add valdition and secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,32 @@
 class User < ApplicationRecord
+  enum role: [:user, :admin]
   has_many :bookings, dependent: :destroy
   has_many :football_pitches, through: :bookings
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /\A
+                        (?=.*[a-z])
+                        (?=.*[A-Z])
+                        (?=.*\d)
+                        (?=.*[@$!%*?&])
+                        [A-Za-z\d@$!%*?&]{8,}\z
+                        /x
+
+  validates :name,
+            presence: true,
+            length: {maximum: Settings.digit.length_50}
+
+  validates :email,
+            presence: true,
+            length: {maximum: Settings.digit.length_255},
+            format: {with: VALID_EMAIL_REGEX},
+            uniqueness: {case_sensitive: false}
+
+  validates :password,
+            presence: true,
+            length: {minimum: Settings.digit.length_8},
+            format: {with: VALID_PASSWORD_REGEX},
+            allow_nil: true
+
+  has_secure_password
 end
